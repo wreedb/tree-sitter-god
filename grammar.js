3028dc0e6698c0b6d02d101330af6c404e53e2ef
Will Reed <wreed@disroot.org>
2025-10-31 22:46:42 -0500

    fix: handle ML strings, operators, add assignment operator

--- grammar.js
+++ grammar.js
@@ -7,7 +7,7 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
-module.exports = grammar({
+export default grammar({
   name: "god",
 
   extras: $ => [
@@ -20,20 +20,88 @@ module.exports = grammar({
     document: $ => seq('{', repeat($.field), '}'),    
     identifier: $ => /[A-Za-z_][A-Za-z0-9_\'\-]*/,
     
+    operator: $ => choice(
+      $.termination,
+      $.assignment,
+      $.negation
+    ),
+    
     element: $ => choice(
       $.string,
       $.number, 
       $.bool,
       $.null,
       $.list,
-      $.map
+      $.map,
+      $.negatedElement
     ),
 
-    field: $ => seq($.identifier, '=', $.element, ';'),
+    termination: $ => token(';'),
+    negation: $ => token('-'),
+    assignment: $ => token('='),
+    
+    negatedElement: $ => seq(
+      $.negation,
+      $.element
+    ),
+    
+    field: $ => seq(
+        $.identifier, // name
+        $.assignment, // =
+        $.element,    // "Will"
+        $.termination // ;
+    ),
+    
     list: $ => seq('[', repeat($.element), ']'),
     map: $ => seq('{', repeat($.field), '}'),
-    string: $ => token(seq( '"', repeat(choice( /[^\"]/, /\\"/, /\\n/, /\\r/, /\\t/ )), '"', )),
-    number: $ => token(/-?(?:\d+(?:\.\d*)?(?:[eE][+-]?\d+)?|\.\d+(?:[eE][+-]?\d+)?)/),
+    
+    string: $ => choice(
+        token(seq(
+          '"',
+          repeat(choice(
+            /[^"]/,
+            /\\"/,
+            /\\n/,
+            /\\r/,
+            /\\t/
+          )),
+          '"',
+        )),
+        token(seq(
+            "''",
+            repeat(choice(
+                /[^']/,           // Any character except single quote
+                /'[^']/,          // Single quote followed by non-quote
+                /'''/,            // Triple quote (escape for '')
+                /''\$/,           // Escape for literal ''
+                /''\\/,           // Escape for backslash
+            )),
+            "''"))),
+    
+    number: $ => token(
+        choice(
+            seq(
+                /\d+/,
+                optional(seq('.', optional(/\d+/))),
+                optional(seq(
+                    /[eE]/,
+                    optional(choice('+', '-')),
+                    /\d+/
+                ))
+            ),
+            seq(
+                '.',
+                /\d+/,
+                optional(seq(
+                    /[eE]/,
+                    optional(choice('+', '-')),
+                    /\d+/
+                ))
+            )
+        )
+    ),
+    
+    // number: $ => token(/-?(?:\d+(?:\.\d*)?(?:[eE][+-]?\d+)?|\.\d+(?:[eE][+-]?\d+)?)/),
     bool: $ => choice('true', 'false'),
     null: $ => 'null',
     comment: $ => /#.*/,
